8a412765d0ca3ad9af7c3e29bf9ae99ab06eb34b
César Guerra <warderer@gmail.com>
2025-09-18 20:14:39 -0600

    ✨ Update App with NewPost and Footer

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import NewPost from './pages/NewPost/NewPost'
 
 function App () {
   return (
-    <>
+    <div className='app'>
+      <main className='app-main'>
+        <div className='container'>
+          <NewPost />
+        </div>
+      </main>
 
-    </>
+      <footer className='app-footer'>
+        <div className='container'>
+          <p>© 2025 Blog de César Guerra. Todos los derechos reservados.</p>
+        </div>
+      </footer>
+    </div>
   )
 }
 
