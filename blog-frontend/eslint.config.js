582b89f45538ae2272c69bc6eef6d6325114333f
César Guerra <warderer@gmail.com>
2025-09-18 19:38:13 -0600

    🔧 ➕ Add neostandard to project

--- blog-frontend/eslint.config.js
+++ blog-frontend/eslint.config.js
@@ -3,8 +3,10 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import neostandard from 'neostandard'
 
 export default defineConfig([
+  ...neostandard(),
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
