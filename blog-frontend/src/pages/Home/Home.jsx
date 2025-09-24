f981cb2fb603dde0f066910c52a827fb48e1c483
César Guerra <warderer@gmail.com>
2025-09-23 19:42:25 -0600

    ✨ Update Home to call API

--- blog-frontend/src/pages/Home/Home.jsx
+++ blog-frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,36 @@
+import { useState, useEffect } from 'react'
+import './home.css'
+
+const getRandomImageUrl = (postId) => {
+  const ImageId = 100 + postId
+  return `https://picsum.photos/id/${ImageId}/600/400`
+}
+
 const Home = () => {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  // Traemos los posts de la API de jsonplaceholder
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        if (!response.ok) {
+          throw new Error('Error al obtener los posts')
+        }
+        const data = await response.json()
+        setPosts(data)
+      } catch (err) {
+        setError(err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchPosts()
+  }, [])
+
   return (
     <h1>Home</h1>
   )
