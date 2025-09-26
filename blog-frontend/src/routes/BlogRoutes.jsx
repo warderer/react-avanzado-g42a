b3d353b907c2d81793b8a693413172df5f73ff54
César Guerra <warderer@gmail.com>
2025-09-25 20:19:30 -0600

    ✨ Protect /newpost Route

--- blog-frontend/src/routes/BlogRoutes.jsx
+++ blog-frontend/src/routes/BlogRoutes.jsx
@@ -1,15 +1,21 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import NewPost from '../pages/NewPost/NewPost'
 import Contact from '../pages/Contact/Contact'
 import Login from '../pages/Login/Login'
 import Register from '../pages/Register/Register'
+import { useAuthContext } from '../hooks/useAuth'
 
 const BlogRoutes = () => {
+  const { isAuth } = useAuthContext()
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/newpost' element={<NewPost />} />
+      <Route
+        path='/newpost'
+        element={isAuth ? <NewPost /> : <Navigate to='/login' replace />}
+      />
       <Route path='/contact' element={<Contact />} />
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
