8512c5e2994a80c0a4c30c8c26f68cdc724ebc83
César Guerra <warderer@gmail.com>
2025-09-25 19:32:39 -0600

    ✨ Add AuthContext implementation

--- blog-frontend/src/context/AuthContext.jsx
+++ blog-frontend/src/context/AuthContext.jsx
@@ -1,14 +1,44 @@
 // 1. Crear el contexto
-import { createContext } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 const AuthContext = createContext()
 
 // 2. Crear el componente del proveedor del contexto (provider)
 
 function AuthProvider ({ children }) {
+  // Lógica de Autenticación
+  const [isAuth, setIsAuth] = useState(false) // ¿Estoy autenticado?
+  const [userPayload, setUserPayload] = useState(null) // Datos del usuario autenticado
+
+  const login = (data) => {
+    localStorage.setItem('userData', JSON.stringify(data))
+    setUserPayload(data)
+    setIsAuth(true)
+  }
+
+  const logout = () => {
+    localStorage.removeItem('userData')
+    setUserPayload(null)
+    setIsAuth(false)
+  }
+
+  useEffect(() => {
+    // Al cargar la app, verificar si hay datos de usuario en el localStorage
+    const data = localStorage.getItem('userData')
+    if (data) {
+      // Si hay datos, convertilos a Objeto de JS y actualizar el estado
+      const parsedData = JSON.parse(data)
+      setUserPayload(parsedData)
+      setIsAuth(true)
+    }
+  }, [])
+
   // Aqui van los datos que quiero compartir de forma global
   const data = {
-
+    isAuth,
+    userPayload,
+    login,
+    logout
   }
 
   return (
