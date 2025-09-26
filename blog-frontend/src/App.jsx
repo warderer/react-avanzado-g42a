17f980281253541696bc854e52f39d6a2c2b2c92
César Guerra <warderer@gmail.com>
2025-09-25 19:36:27 -0600

    ✨ Add AuthProvider to App component

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -1,26 +1,29 @@
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import BlogRoutes from './routes/BlogRoutes'
 import Header from './components/Header/Header'
 import './App.css'
 
 function App () {
   return (
-    <BrowserRouter>
-      <Header />
-      <div className='app'>
-        <main className='app-main'>
-          <div className='container'>
-            <BlogRoutes />
-          </div>
-        </main>
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <div className='app'>
+          <main className='app-main'>
+            <div className='container'>
+              <BlogRoutes />
+            </div>
+          </main>
 
-        <footer className='app-footer'>
-          <div className='container'>
-            <p>© 2025 Blog de César Guerra. Todos los derechos reservados.</p>
-          </div>
-        </footer>
-      </div>
-    </BrowserRouter>
+          <footer className='app-footer'>
+            <div className='container'>
+              <p>© 2025 Blog de César Guerra. Todos los derechos reservados.</p>
+            </div>
+          </footer>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
