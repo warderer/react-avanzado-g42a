3ee13478b67de363f3ef2c41187079147d9e4cf4
César Guerra <warderer@gmail.com>
2025-10-02 20:15:08 -0600

    ✨ Add CORS support to backend server

--- blog-backend/server.js
+++ blog-backend/server.js
@@ -1,6 +1,7 @@
 // #1 Importar Express
 import express from 'express'
 import postsData from './data/posts.json' with { type: 'json' }
+import cors from 'cors'
 
 // #2a Crear una instancia de Express
 const app = express()
@@ -8,6 +9,12 @@ const port = process.env.PORT || 3000
 
 // #2b Configurar express para que entienda JSON
 app.use(express.json())
+
+// #2c Configurar express para recibir datos de formularios
+app.use(express.urlencoded({ extended: true }))
+
+// #2d Configurar CORS para permitir solicitudes desde el frontend
+app.use(cors())
 
 // #3 Definir rutas
 app.get('/', (req, res) => {
